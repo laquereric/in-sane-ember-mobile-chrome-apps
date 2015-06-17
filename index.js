ce1efda6d449cd4be7ebc97607f58ebda8264798
Eric Laquer <LaquerEric@gmail.com>
2015-06-17 14:19:13 -0400

    Copied ember-cli-mobile-chrome-apps commit 88226c894dd0862a281f4574e79af2059578205b comment: Cleanup requires

---
+++ index.js
@@ -0,0 +1,19 @@
+var path = require('path');
+
+module.exports = {
+  name: 'ember-cli-mobile-chrome-apps',
+
+  included: function(app) {
+    if (process.argv[2] === 'chrome:build') {
+      app.options.fingerprint.enabled = false;
+    }
+  },
+
+  includedCommands: function() {
+    return require('./lib/commands');
+  },
+
+  blueprintsPath: function() {
+    return path.join(__dirname, 'blueprints');
+  }
+};
